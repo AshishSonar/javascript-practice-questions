3456dd7bd1ec043a75df24db643870f4574d1c09
AshishSonar <ashish08sonar@gmail.com>
2022-11-15 22:20:34 +0530

    solved problems

--- question27.js
+++ question27.js
@@ -6,7 +6,7 @@
 //     - Find the range of the ages(max minus min)
 //     - Compare the value of (min - average) and (max - average), use abs() method
 
-const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24, 100]
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
 ages.sort(function(a, b){return a - b})
 console.log(ages)
@@ -26,3 +26,28 @@ function myArrayMin(arr) {
   let minage = myArrayMin(ages)
 console.log(`minimum age is ${minage}`)
 
+
+// find the median
+const median = arr => {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b);
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
+
+console.log(median(ages))
+
+//find the average
+const sum = ages.reduce((a, b) => a + b, 0);
+const avg = (sum / ages.length) || 0;
+
+console.log(`The sum is: ${sum}. The average is: ${avg}.`);
+
+//range of ages
+let range = maxage - minage
+console.log(`age is ranging between ${range} years`)
+
+//Compare the value of (min - average) and (max - average), use abs() method
+let compare = Math.abs(minage - avg);
+let compare2 = Math.abs(maxage - avg);
+
+console.log(compare2)
